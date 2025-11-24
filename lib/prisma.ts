e3fa2ad5f2baed3e36544ae328f24dceb864ec3b
JuliantoLauw <juliantolauw@gmail.com>
2025-11-24 20:11:13 +0700

    Fix prisma client path for vercel production

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,20 @@
 import { PrismaClient } from "@prisma/client";
-import { log } from "console";
+import path from "path";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const dbPath = path.join(process.cwd(), "prisma/dev.db");
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({log: ['query']});
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+    datasources: {
+      db: {
+        url: process.env.NODE_ENV === "production" 
+             ? `file:${dbPath}` 
+             : process.env.DATABASE_URL,
+      },
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
